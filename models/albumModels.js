6d16bc1e13456ad58e58e5a962bec511642ab9b7
Marcelo <91795716+MarMastro@users.noreply.github.com>
2023-10-05 21:03:06 -0300

    Update albumModels.js

--- models/albumModels.js
+++ models/albumModels.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
-import validator from "validator";
 
-const albumsCollection = "albums"
+const albumsCollection = "albums";
 
 const album = new mongoose.Schema(
      {
